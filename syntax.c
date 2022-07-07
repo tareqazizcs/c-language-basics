94dec8f963c709efb238077b7a79e1abf2f60739
tareqazizcs <tareqaziz.dev@gmail.com>
2022-07-07 03:40:11 +0200

    repo initialized

---
+++ syntax.c
@@ -0,0 +1,9 @@
+/* a "C program" always starts with a main function */
+
+#include <stdio.h>  //  here #include <stdio.h> is a header file library that lets us work with input and output functions
+
+int main() {        // main function starts here
+  printf("Hello World!");
+  return 0;         // return 0 ends the main() function 
+}
+// function ends with the curly braces
