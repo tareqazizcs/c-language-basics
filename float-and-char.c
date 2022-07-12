9a0ec7d2a2a84ea634564719c6f7de0201b239d9
tareqazizcs <tareqaziz.dev@gmail.com>
2022-07-12 21:50:01 +0200

    float-and-char.c is added

---
+++ float-and-char.c
@@ -0,0 +1,21 @@
+/* To print a floatting number we need to use format specifier '%f' */
+#include <stdio.h>
+
+int main(){
+    float a = 1.12;
+    printf("The value of a is %f", a);
+
+    return 0;
+}
+
+/* To print a character we need to use format specifier '%c' */
+
+#include <stdio.h>
+int main(){
+    char firstChar = 't';
+    printf("The first character is %c", firstChar);
+
+    return 0;
+}
+
+// Wthout format specifier these will not be printed.
