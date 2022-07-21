1aed022befdab4bb4075e0b0aec70211a3895601
tareqazizcs <tareqaziz.dev@gmail.com>
2022-07-21 22:00:53 +0200

    data-type.c added

---
+++ data-type.c
@@ -0,0 +1,28 @@
+/* Data type defines which type of data is stored to the memory buffer. */
+
+/* 
+Data type            |       Size in bytes
+
+char or signed char                 1
+
+Unsigned char                       1
+
+int or signed int                   2 
+
+Unsigned int                        2
+
+Short int or unsigned short int     2
+
+signed short int                    2
+
+Long int or signed long int         4
+
+Unsigned long int                   4
+
+float                               4
+
+double                              8
+
+long double                         10
+
+*/
