211d3f21898269a5c479f3daf6997708f3fd0110
tareqazizcs <tareqaziz.dev@gmail.com>
2022-08-10 03:39:27 +0200

    conditional-instructions.c added

---
+++ conditional-instructions.c
@@ -0,0 +1,18 @@
+/* A program can be executed based on given conditions. */
+
+/* 
+Decision making instructions in C -
+- If Else statements
+- Switch case statements
+
+Syntax -
+
+if (condition) {
+  =>block of code to be executed if the condition is true
+} else {
+  => block of code to be executed if the condition is false
+}
+
+
+*/
+
